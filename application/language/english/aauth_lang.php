27fd0a88544ead19485082ff614af6813b233391
REJack <rejack@hotmail.de>
2014-08-06 04:07:29 +0200

    Create aauth_lang.php

---
+++ application/language/english/aauth_lang.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['verification_subject'] = 'Account Vertification';
+$lang['reset'] = 'Pasword Reset';
+
+// error mesages
+// change to your language
+
+$lang['email_taken'] = 'E-mail is already taken';
+$lang['email_invalid'] = 'E-mail invalid';
+$lang['pass_invalid'] = 'Password invalid';
+$lang['name_invalid'] = 'Name invalid';
+$lang['code'] = 'Your code is: ';
+$lang['link'] = ' or you can copy and paste falowing link http://localhost/vert/';
+
+$lang['remind'] = 'If you want to reset your password click the copy and go the link below http://localhost/reset/';
+$lang['new_password'] = 'Your new password is : ';
+
+// no access
+$lang['no_access'] = 'You dont have access.';
+
+//
+$lang['wrong'] = 'E-mail or Password is wrong.';
+$lang['exceeded'] = 'Login try limit exceeded.';
+$lang['no_user'] = 'User not Exist';
+$lang['not_verified'] = 'Please verify your account.';
+$lang['group_exist'] = 'Group already exists';
+$lang['no_group'] = 'Group doesn\'t exists';
+$lang['self_pm'] = 'It is not reasonable to send pm to yourself :)';
+$lang['no_pm'] = 'Pm not found';
+
+//info
+$lang['already_member'] = 'User already member of group';
+$lang['already_perm'] = 'Permission name already existed';
