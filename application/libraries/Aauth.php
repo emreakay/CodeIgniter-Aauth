9ead7557c9ff7892e97bd26aa2940370e7038d83
Vipin K. Singh <vipinksindia@gmail.com>
2015-10-17 11:32:24 +0530

    Another approach for aauth_error_login_failed
    keeping the old logic, but using proper lang key "aauth_error_no_user"

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -216,12 +216,7 @@ class Aauth {
 		$query = $this->aauth_db->get($this->config_vars['users']);
 		
 		if($query->num_rows() == 0){
-			if( $this->config_vars['login_with_name'] == TRUE){
-				$this->error($this->CI->lang->line('aauth_error_username_invalid'));
-			} else {
-				$this->error($this->CI->lang->line('aauth_error_email_invalid'));
-			}
-			//$this->error($this->CI->lang->line('aauth_error_login_failed'));
+			$this->error($this->CI->lang->line('aauth_error_no_user'));
 			return FALSE;
 		}
 		
