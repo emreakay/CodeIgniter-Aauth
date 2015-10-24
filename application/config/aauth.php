36bce1e8401f3021d47f14bf75496770d97bbc2d
Steve <steve@castoretpollux.com>
2015-10-24 16:18:57 +0200

    Update config file for readability and comprehension

--- application/config/aauth.php
+++ application/config/aauth.php
@@ -1,85 +1,124 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 /*
-  | -------------------------------------------------------------------
-  |  Aauth Config
-  | -------------------------------------------------------------------
-  | A library Basic Authorization for CodeIgniter 2.x
- */
+| -------------------------------------------------------------------
+| Aauth Config
+| -------------------------------------------------------------------
+| A library Basic Authorization for CodeIgniter 2+
+|
+| -------------------------------------------------------------------
+| EXPLANATION
+| -------------------------------------------------------------------
+|
+|	See http://codeigniter-aauth-test.readthedocs.org/en/latest/
+|	for more details and explainations
+|
+|
+| 	['no_permission']                  	If user don't have permisssion to see the page he will be redirected the page spesificed.
+|
+| 	['admin_group']                    	Name of admin group
+| 	['default_group']                  	Name of default group, the new user is added in it
+| 	['public_group']                   	Public group , people who not logged in
+|
+| 	['db_profile']                     	The configuration database profile (see config/database.php)
+|
+| 	['users']                          	The table which contains users
+| 	['groups']                         	The table which contains groups
+| 	['user_to_group']                  	The table which contains join of users and groups
+| 	['perms']                          	The table which contains permissions
+| 	['perm_to_group']                  	The table which contains permissions for groups
+| 	['perm_to_user']                   	The table which contains permissions for users
+| 	['pms']                            	The table which contains private messages
+| 	['system_variables']               	The table which contains Aauth system variables
+| 	['user_variables']                 	The table which contains users variables
+|
+| 	['remember']                       	Remember time elapsed after connecting and automatic LogOut
+|
+| 	['max']                            	Maximum char long for Password
+| 	['min']                            	Minimum char long for Password
+|
+| 	['valid_chars']                    	Valid chars for username. Non alphanumeric characters that are allowed by default
+|
+| 	['ddos_protection']                	If it is true, the user will be banned temporary when he exceed the login 'try'
+|
+| 	['recaptcha_active']               	Enable reCAPTCHA (see www.google.com/recaptcha/admin)
+| 	['recaptcha_login_attempts']       	:
+| 	['recaptcha_siteKey']              	The reCAPTCHA siteKey
+| 	['recaptcha_secret']               	The reCAPTCHA secretKey
+|
+| 	['totp_active']                    	The Time-based One-time Password Algorithm
+| 	['totp_only_on_ip_change']         	TOTP only on IP Change
+| 	['totp_reset_over_reset_password'] 	TOTP reset over reset Password
+|
+| 	['max_login_attempt']              	Login attempts time interval (default 20 times in one hour)
+|
+| 	['login_with_name']                	Login Identificator, if TRUE username needed to login else email address.
+|
+| 	['use_cookies']                    	FALSE only on CI3
+|
+| 	['email']                          	Sender email address, used for remind_password, send_verification and reset_password
+| 	['name']                           	Sender name, used for remind_password, send_verification and reset_password
+|
+| 	['verification']                   	User Verification, if TRUE sends a verification email on account creation.
+| 	['verification_link']              	Link for verification without site_url or base_url
+| 	['reset_password_link']            	Link for reset_password without site_url or base_url
+|
+*/
+$config_aauth = array();
 
+$config_aauth["default"] = array(
+	['no_permission']                  = FALSE,
 
-// Config variables
+	['admin_group']                    = 'admin',
+	['default_group']                  = 'default',
+	['public_group']                   = 'public',
 
-// if user don't have permisssion to see the page he will be
-// redirected the page spesificed below
-$config['aauth']['no_permission'] = FALSE;
-//name of admin group
-$config['aauth']['admin_group'] = 'admin';
-//name of default group, the new user is added in it
-$config['aauth']['default_group'] = 'default';
-// public group , people who not logged in
-$config['aauth']['public_group'] = 'public';
-// The table which contains users
-$config['aauth']['db_profile'] = 'default';
+	['db_profile']                     = 'default',
 
-$config['aauth']['users'] = 'aauth_users';
-// the group table
-$config['aauth']['groups'] = 'aauth_groups';
-// 
-$config['aauth']['user_to_group'] = 'aauth_user_to_group';
-// permitions
-$config['aauth']['perms'] = 'aauth_perms';
-// perms to group
-$config['aauth']['perm_to_group'] = 'aauth_perm_to_group';
-// perms to group
-$config['aauth']['perm_to_user'] = 'aauth_perm_to_user';
-// pm table
-$config['aauth']['pms'] = 'aauth_pms';
-// system variables
-$config['aauth']['system_variables'] = 'aauth_system_variables';
-// user variables
-$config['aauth']['user_variables'] = 'aauth_user_variables';
+	['users']                          = 'aauth_users',
+	['groups']                         = 'aauth_groups',
+	['user_to_group']                  = 'aauth_user_to_group',
+	['perms']                          = 'aauth_perms',
+	['perm_to_group']                  = 'aauth_perm_to_group',
+	['perm_to_user']                   = 'aauth_perm_to_user',
+	['pms']                            = 'aauth_pms',
+	['system_variables']               = 'aauth_system_variables',
+	['user_variables']                 = 'aauth_user_variables',
 
-// remember time
-$config['aauth']['remember'] = ' +3 days';
+	['remember']                       = ' +3 days',
 
-// pasword maximum char long
-$config['aauth']['max'] = 13;
-// pasword minimum char long
-$config['aauth']['min'] = 5;
+	['max']                            = 13,
+	['min']                            = 5,
 
-// non alphanumeric characters that are allowed in a name
-$config['aauth']['valid_chars'] = array();
+	['valid_chars']                    = array(),
 
-// ddos protection,
-//if it is true, the user will be banned temporary when he exceed the login 'try'
-$config['aauth']['ddos_protection'] = true;
+	['ddos_protection']                = true,
 
-$config['aauth']['recaptcha_active'] = false;
-$config['aauth']['recaptcha_login_attempts'] = 4;
-$config['aauth']['recaptcha_siteKey'] = '';
-$config['aauth']['recaptcha_secret'] = '';
+	['recaptcha_active']               = false,
+	['recaptcha_login_attempts']       = 4,
+	['recaptcha_siteKey']              = '',
+	['recaptcha_secret']               = '',
 
-$config['aauth']['totp_active'] = false;
-$config['aauth']['totp_only_on_ip_change'] = false;
-$config['aauth']['totp_reset_over_reset_password'] = false;
-// login attempts time interval
-// default 20 times in one hour
-$config['aauth']['max_login_attempt'] = 10;
+	['totp_active']                    = false,
+	['totp_only_on_ip_change']         = false,
+	['totp_reset_over_reset_password'] = false,
 
-// to register email verifitaion need? true / false
-$config['aauth']['verification'] = false;
+	['max_login_attempt']              = 10,
 
-$config['aauth']['login_with_name'] = false;
-$config['aauth']['use_cookies'] = true; // FALSE only on CI3
+	['login_with_name']                = false,
 
-// system email.
-$config['aauth']['email'] = 'admin@admin.com';
-$config['aauth']['name'] = 'Emre Akay';
+	['use_cookies']                    = true,
 
-// Link for verification without site_url or base_url
-$config['aauth']['verification_link'] = '/account/verification/';
-// Link for reset_password without site_url or base_url
-$config['aauth']['reset_password_link'] = '/account/reset_password/';
+	['email']                          = 'admin@admin.com',
+	['name']                           = 'Emre Akay',
+
+	['verification']                   = false,
+	['verification_link']              = '/account/verification/',
+	['reset_password_link']            = '/account/reset_password/'
+);
+
+$config['aauth'] = $config_aauth['default'];
 
 /* End of file aauth.php */
 /* Location: ./application/config/aauth.php */
