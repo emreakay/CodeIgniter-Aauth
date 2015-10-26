80977fe87742a32da238baa6410499cc5de80a31
Emre Akay <emreakayfb@gmail.com>
2015-10-26 09:54:15 +0200

    Update Aauth.php

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -13,7 +13,7 @@
  *
  * @copyright 2014-2015 Emre Akay
  *
- * @version 2.4.1
+ * @version 2.4.2
  *
  * @license LGPL
  * @license http://opensource.org/licenses/LGPL-3.0 Lesser GNU Public License
