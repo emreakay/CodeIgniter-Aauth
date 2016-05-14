4c3aec4b7c86fbd08b430a1efd7f6e8989f4272a
REJack <info@rejack.de>
2016-05-14 14:31:46 +0200

    fixed `strtotime()` parameter

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -639,7 +639,7 @@ class Aauth {
 
 		$data = array();
 
-		if (strtotime($row->last_login_attempt) > strtotime($this->config_vars['max_login_attempt_per_minutes'])) {
+		if (strtotime($row->last_login_attempt) > strtotime("-"+$this->config_vars['max_login_attempt_per_minutes']+" minutes")) {
 			$data['login_attempts'] = $row->login_attempts + 1;
 
 			if($this->config_vars['update_last_login_attempt']){
