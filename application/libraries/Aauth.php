952f3ebe46cbe5a5297e2668c4d8153048d4845f
REJack <info@rejack.de>
2016-05-14 12:57:51 +0200

    changed version to 2.5.0-alpha

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -13,7 +13,7 @@
  *
  * @copyright 2014-2015 Emre Akay
  *
- * @version 2.4.7
+ * @version 2.5.0-alpha
  *
  * @license LGPL
  * @license http://opensource.org/licenses/LGPL-3.0 Lesser GNU Public License
