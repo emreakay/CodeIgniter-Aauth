baa0ef1a1ada04310451e34b05df1f3641f9c260
2329697501 <2329697501@qq.com>
2016-05-21 10:57:52 +0800

    Update Aauth.php
    when get_perm_id method return null ,SELECT * FROM `ccc_aauth_perms` WHERE `name` IS NULL

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -1575,7 +1575,9 @@ class Aauth {
 		}
 
 		$perm_id = $this->get_perm_id($perm_par);
-		
+		if($perm_id===null){
+			return false;
+		}
 		$query = $this->aauth_db->where('perm_id', $perm_id);
 		$query = $this->aauth_db->where('user_id', $user_id);
 		$query = $this->aauth_db->get( $this->config_vars['perm_to_user'] );
@@ -1607,7 +1609,9 @@ class Aauth {
 	public function is_group_allowed($perm_par, $group_par=FALSE){
 
 		$perm_id = $this->get_perm_id($perm_par);
-
+		if($perm_id===null){
+			return false;
+		}
 		// if group par is given
 		if($group_par != FALSE){
 
@@ -1782,7 +1786,7 @@ class Aauth {
 	 */
 	public function get_perm_id($perm_par) {
 
-		if( is_numeric($perm_par) ) { return $perm_par; }
+		if( is_numeric($perm_par) || ($perm_par === null) ) { return $perm_par; }
 
 		$query = $this->aauth_db->where('name', $perm_par);
 		$query = $this->aauth_db->get($this->config_vars['perms']);
