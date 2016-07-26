05e47288c7b5f71b7d23f91fc1b6c028c6d51e2e
Raphael Jackstadt <info@rejack.de>
2016-07-26 10:20:42 +0200

    updated version tag

--- application/libraries/Aauth.php
+++ application/libraries/Aauth.php
@@ -13,7 +13,7 @@
  *
  * @copyright 2014-2016 Emre Akay
  *
- * @version 2.5.8
+ * @version 2.5.10
  *
  * @license LGPL
  * @license http://opensource.org/licenses/LGPL-3.0 Lesser GNU Public License
