baab5204bc7895b7cc23b584827c04fad72685bb
REJack <info@rejack.de>
2018-11-01 22:58:47 +0100

    added Controller/Home.php

---
+++ Controllers/Home.php
@@ -0,0 +1,12 @@
+<?php namespace Magefly\Aauth\Controllers;
+
+use CodeIgniter\Controller;
+use Magefly\Aauth\Config\Aauth as AauthConfig;
+
+class Home extends Controller
+{
+	public function index()
+	{
+
+	}
+}
