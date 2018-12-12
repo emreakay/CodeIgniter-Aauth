452b60be012a7a2e783bd65a62b3285aeb47bba0
REJack <info@rejack.de>
2018-12-12 14:09:57 +0100

    updated LoginTest

--- tests/Aauth/Libraries/Aauth/LoginTest.php
+++ tests/Aauth/Libraries/Aauth/LoginTest.php
@@ -141,7 +141,7 @@ class LoginTest extends CIDatabaseTestCase
 			'loggedIn' => true,
 		]);
 		$this->assertTrue($this->library->isLoggedIn());
-		$session->remove('user');
+		$session->set('user', []);
 
 		helper('text');
 		$session        = $this->getInstance();
